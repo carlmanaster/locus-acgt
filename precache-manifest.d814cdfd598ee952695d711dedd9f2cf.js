947b67d2bf3454326d1a6f9e584a8179ecda92ca
Carl Manaster <manaster@pobox.comr>
2018-11-13 11:28:36 -0800

    Updates

---
+++ precache-manifest.d814cdfd598ee952695d711dedd9f2cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4d2442319fd554a962f9",
+    "url": "/locus-acgt/static/js/runtime~main.4d244231.js"
+  },
+  {
+    "revision": "91d477473f1b816a4afc",
+    "url": "/locus-acgt/static/js/main.91d47747.chunk.js"
+  },
+  {
+    "revision": "5ba7e5a978ad28912a8f",
+    "url": "/locus-acgt/static/js/1.5ba7e5a9.chunk.js"
+  },
+  {
+    "revision": "91d477473f1b816a4afc",
+    "url": "/locus-acgt/static/css/main.eb9e4d55.chunk.css"
+  },
+  {
+    "revision": "5ba7e5a978ad28912a8f",
+    "url": "/locus-acgt/static/css/1.ead2c8e7.chunk.css"
+  },
+  {
+    "revision": "fc488e0f29762fba3190c7b13828d64a",
+    "url": "/locus-acgt/index.html"
+  }
+];
